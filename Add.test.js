7940dee33a4d07d14ae813f7282b5158f63708fa
Vivek Chhariror <vivekchhariror@gmail.com>
2024-05-14 21:47:39 +0530

    Todo test cases for simple string calculator

---
+++ Add.test.js
@@ -0,0 +1,11 @@
+// const add = require('./Add')
+
+describe("Add function", () => {
+	test.todo('Empty string should return 0')
+
+	test.todo('Single number should return that number')
+
+	test.todo('Two numbers should return their sum')
+
+	test.todo('Handle any amount of numbers')
+})
